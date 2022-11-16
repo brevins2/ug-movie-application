d2c6965821eae4087aa9700dd62759f789634842
Kiggundu Brevin <kiggundubrevin99@gmail.com>
2022-11-16 22:18:13 +0300

    customer edits

--- ug-movie-application/src/app/developer/customers/customers.component.ts
+++ ug-movie-application/src/app/developer/customers/customers.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
 
 export interface PeriodicElement {
   name: string;
@@ -8,6 +11,18 @@ export interface PeriodicElement {
   delete: string;
   edit: string;
 }
+
+export class Users{
+  constructor(
+    public id: number,
+    public file: string,
+    public email: string,
+    public password: string,
+    public confirmPassword: string,
+    public check: boolean
+  ){}
+}
+
 
 const ELEMENT_DATA: PeriodicElement[] = [
   {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H', delete: 'delete', edit: 'edit'},
@@ -39,6 +54,15 @@ const ELEMENT_DATA: PeriodicElement[] = [
 })
 export class CustomersComponent implements OnInit {
 
+ user: Users[] =[];
+  closeResult = '';
+  signUpForm = new FormGroup({
+    email: new FormControl(''),
+    password: new FormControl(''),
+    confirmPassword: new FormControl(''),
+    file: new FormControl(''),
+    check: new FormControl('')
+  });
 
 	displayedColumns: string[] = ['position', 'name', 'weight', 'symbol', 'delete', 'edit'];
   	dataSource = ELEMENT_DATA;
@@ -46,6 +70,51 @@ export class CustomersComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+
+  open(content: any) {
+    this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
+      this.closeResult = `Closed with: ${result}`;
+    }, (reason) => {
+      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+    });
+  }
+
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return `with: ${reason}`;
+    }
+  }
+
+  // user modal
+  getCurrentData(){
+    this.service.getCurrentUserData(this.route.snapshot.params['id']).subscribe((result: any) => {
+      this.signUpForm = new FormGroup({
+      email: new FormControl(result['email']),
+      password: new FormControl(result['password']),
+      confirmPassword: new FormControl(result['confirmPassword']),
+      file: new FormControl(result['file']),
+      check: new FormControl(result['check'])
+    });
+  });
+  }
+
+
+
+    signUp(){
+    this.service.updateUser(this.route.snapshot.params['id'], this.signUpForm.value).subscribe((result) => {
+      this.signUpForm.reset();
+      return result;
+    });
+  }
+
+  cancel(){
+    this.router.navigate(['/admin/user']);
   }
 
 }
