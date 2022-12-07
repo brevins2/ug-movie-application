6908e2e52a971966c5c0e4b22523740ddefa1def
Kiggundu Brevin <kiggundubrevin99@gmail.com>
2022-12-07 18:30:52 +0300

    server link edits

--- ug-movie-application/src/app/developer/moviesback/moviesback.component.ts
+++ ug-movie-application/src/app/developer/moviesback/moviesback.component.ts
@@ -1,34 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
-export interface PeriodicElement {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
+export class Movies{
+  constructor(
+    public ID: number,
+    public Title: string,
+    public File: string,
+    public Genre: string,
+    public Producer: string,
+    public Details: string
+  ){}
 }
-
-const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
-];
 
 @Component({
   selector: 'app-moviesback',
@@ -37,12 +19,20 @@ const ELEMENT_DATA: PeriodicElement[] = [
 })
 export class MoviesbackComponent implements OnInit {
 
-	displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
-  	dataSource = ELEMENT_DATA;
+  movies: Movies[] = [];
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
+    this.getMovies();
+  }
+
+  getMovies(){
+    this.http.get<any>('http://localhost:8080/Movies').subscribe(
+      response=>{
+        this.movies = response;
+        console.log(response);
+      });
   }
 
 }
