16973c0f7960bd4b9cc3f4ce1326a890e37103d4
Kiggundu Brevin <kiggundubrevin99@gmail.com>
2022-12-31 23:15:00 +0300

    style back-end

--- ug-movie-application/src/app/security/Movie/movie.component.ts
+++ ug-movie-application/src/app/security/Movie/movie.component.ts
@@ -2,13 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
 
-export interface FileUpload {
-  key: string;
-  name: string;
-  url: string;
-  file: File;
-}
-
 @Component({
   selector: 'app-movie',
   templateUrl: './movie.component.html',
@@ -24,33 +17,12 @@ export class MovieComponent implements OnInit {
 		Producer: new FormControl(''),
 		Details: new FormControl('')
 	});
-  private basePath = '/uploads';
 
-  constructor(private http: HttpClient, file: File, private db: AngularFireDatabase, private storage: AngularFireStorage) {
-    this.file = file;
+  constructor(private http: HttpClient) {
   }
 
   ngOnInit(): void {
   }
-  pushFileToStorage(fileUpload: FileUpload): Observable<number> {
-  const filePath = `${this.basePath}/${fileUpload.file.name}`;
-  const storageRef = this.storage.ref(filePath);
-  const uploadTask = this.storage.upload(filePath, fileUpload.file);
-
-  uploadTask.snapshotChanges().pipe(
-    finalize(() => {
-      storageRef.getDownloadURL().subscribe(downloadURL => {
-        fileUpload.url = downloadURL;
-        fileUpload.name = fileUpload.file.name;
-        this.saveFileData(fileUpload);
-        });
-      })
-    ).subscribe();
-
-    return uploadTask.percentageChanges();
-  }
-
-  uploadTask.snapshotChanges().pipe(finalize(() => this.downloadURL = fileRef.getDownloadURL() )).subscribe();
 
   save() {
     this.http.post<any>('http://localhost:8080/add/Movie', this.RegisterForm.value).subscribe(res=>{
