3c6f92bbce2cf11625ba9ae95e1aacf970d9bfef
Kiggundu Brevin <kiggundubrevin99@gmail.com>
2023-01-10 17:12:32 +0300

    database edit

--- ug-movie-application/src/app/security/Movie/movie.component.ts
+++ ug-movie-application/src/app/security/Movie/movie.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl } from '@angular/forms';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpEventType } from '@angular/common/http';
 import { ServeService } from 'src/app/Services/serve.service';
 import { Genre, Producer } from 'src/app/interface';
 
@@ -21,6 +21,7 @@ export class MovieComponent implements OnInit {
 	});
   genres: Genre[] = [];
   producers: Producer[] = [];
+  selectedFile!: File; 
 
   constructor(private http: HttpClient, private serve: ServeService) {
   }
@@ -34,6 +35,42 @@ export class MovieComponent implements OnInit {
       this.producers = data.data;
     });
   }
+
+  onFileUpload(event: any){
+    this.selectedFile = <File>event.target.files[0];
+    this.RegisterForm.patchValue({
+      // fileSource : File
+    });
+  }
+
+  get f(){
+    return this.RegisterForm.controls;
+  }
+
+  onUpload(){
+    // const formData = new FormData();
+    // formData.append('image', this.RegisterForm.get('fileSource').value);
+   
+    // this.http.post('http://localhost:3000/Phones', formData)
+    //   .subscribe(res => {
+    //     console.log(res);
+    //     alert('Uploaded Successfully.');
+    //   })
+    if(this.selectedFile != null){
+      const fd = new FormData();
+      fd.append('image', this.selectedFile, this.selectedFile.name);
+      this.http.post("http://localhost:3000/Phones", fd, {reportProgress: true,
+      observe: 'events'}).subscribe(event => {
+        if(event.type === HttpEventType.UploadProgress){
+          // console.log('Upload Progress: '+ Math.round(event.loaded / event.total * 100));
+          console.log('done');
+        }
+        else if(event.type === HttpEventType.Response){
+          console.log(event);
+        }
+    });
+  }
+}
 
   save() {
     this.http.post<any>('http://localhost:8080/add/Movie', this.RegisterForm.value).subscribe(res=>{
